8b5de1dd12515d28ed5177da787f019044a89bd5
Yinxia Shi <yancy.syx@gmail.com>
2024-09-29 00:50:07 -0700

    Finish lab

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -11,7 +11,7 @@ export default function Labs() {
       <h1>Labs</h1>
       <TOC />
       <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
+        <Route path="/" element={<Navigate to="Lab2" />} />
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
